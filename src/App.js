0e70757fbb05805b50e22677a56063be05bf7c3f
dieum512 <akonkwadieum222@gmail.com>
2023-09-28 12:51:52 +0200

    Wrap the app in the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Greeting from "./components/Greeting";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Greeting/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </Router>
   );
 }
 
