5b88381ac2c33eee6e307adf6283f166267a198b
dieum512 <akonkwadieum222@gmail.com>
2023-09-28 11:35:43 +0200

    Create the redux of the app

---
+++ src/redux/greeting/greetingSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchGreetings = createAsyncThunk(
+  'greetings/fetchGreetings',
+  async () => {
+    const response = await axios.get('http://localhost:3000/api/greetings/index');
+    return response.data;
+  }
+);
+
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState: {
+    greeting: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreetings.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchGreetings.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.greeting = action.payload;
+      })
+      .addCase(fetchGreetings.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default greetingsSlice.reducer;
